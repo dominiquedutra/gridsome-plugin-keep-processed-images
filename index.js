1c7f69e68f94ca5d5e9204a8d51295f410e3ce4b
Dominique Dutra <ddutra@fibersals.com.br>
2019-07-07 14:06:23 -0300

    Keep images between builds

---
+++ index.js
@@ -0,0 +1,24 @@
+const fs = require('fs-extra')
+
+module.exports = function (api) {
+  api.afterBuild(() => {
+		copyFiles(api.config.imagesDir, `${api.config.imageCacheDir}/`)
+  })
+  api.beforeBuild(() => {
+		moveFiles(`${api.config.imageCacheDir}/`, api.config.imagesDir)
+  })
+}
+
+async function copyFiles (source, dest) {
+  try {
+    await fs.copy(source, dest)
+  } catch (err) {
+  }
+}
+
+async function moveFiles (source, dest) {
+  try {
+    await fs.move(source, dest)
+  } catch (err) {
+  }
+}
